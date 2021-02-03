676be72163631ff89af1e0188824ba703a5ba20b
weiwei163@foxmail.com <ylqchengwei5566~>
2021-02-03 18:36:35 +0800

    add: 支持log外部写入

--- src/Log.php
+++ src/Log.php
@@ -3,9 +3,21 @@
 namespace teamones;
 
 use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
-use teamones\process\Etcd;
 
+/**
+ * Class Redis
+ * @package support
+ *
+ * @method static void log($level, $message, array $context = [])
+ * @method static void debug($message, array $context = [])
+ * @method static void info($message, array $context = [])
+ * @method static void notice($message, array $context = [])
+ * @method static void warning($message, array $context = [])
+ * @method static void error($message, array $context = [])
+ * @method static void critical($message, array $context = [])
+ * @method static void alert($message, array $context = [])
+ * @method static void emergency($message, array $context = [])
+ */
 class Log
 {
     /**
@@ -19,10 +31,24 @@ class Log
      */
     public static function channel($name = 'default')
     {
+        $configs = config('monolog', []);
+        if (empty($configs)) {
+            return null;
+        }
+
+        if (class_exists('\support\bootstrap\Log') && isset(\support\bootstrap\Log::$_instance['default'])) {
+            // 外面配置了Log 句柄直接获取
+            return \support\bootstrap\Log::$_instance['default'];
+        }
+
         if (empty(static::$_instance[$name])) {
-            static::$_instance[$name] = new Logger($name);
-            $logPath = Etcd::$etcdConfig['log'] ?? __DIR__ . '/log';
-            static::$_instance[$name]->pushHandler(new StreamHandler($logPath, Logger::ERROR));
+            foreach ($configs as $channel => $config) {
+                $logger = static::$_instance[$channel] = new Logger($channel);
+                foreach ($config['handlers'] as $handler_config) {
+                    $handler = new $handler_config['class'](... \array_values($handler_config['constructor']));
+                    $logger->pushHandler($handler);
+                }
+            }
         };
 
         return static::$_instance[$name];
