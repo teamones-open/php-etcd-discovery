575693b0e60da281897464c13734fcc57bc22567
weiwei163@foxmail.com <ylqchengwei5566~>
2021-02-05 19:49:35 +0800

    add: rpc 客户端注释

--- src/rpc/Client.php
+++ src/rpc/Client.php
@@ -4,18 +4,19 @@ namespace teamones\rpc;
 
 use teamones\breaker\Breaker;
 
+
+//$client = stream_socket_client('tcp://127.0.0.1:8888');
+//$request = [
+//    'class'   => 'User',
+//    'method'  => 'get',
+//    'args'    => [100], // 100 是 $uid
+//];
+//fwrite($client, json_encode($request)."\n"); // text协议末尾有个换行符"\n"
+//$result = fgets($client, 10240000);
+//$result = json_decode($result, true);
+//var_export($result);
+
 class Client
 {
 
 }
-
-$client = stream_socket_client('tcp://127.0.0.1:8888');
-$request = [
-    'class'   => 'User',
-    'method'  => 'get',
-    'args'    => [100], // 100 是 $uid
-];
-fwrite($client, json_encode($request)."\n"); // text协议末尾有个换行符"\n"
-$result = fgets($client, 10240000);
-$result = json_decode($result, true);
-var_export($result);
